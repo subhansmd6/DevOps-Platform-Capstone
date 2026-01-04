ba24b870f82cc8cff527ae224f2b2c8692ce0b57
subhansmd6 <93197485+subhansmd6@users.noreply.github.com>
2026-01-04 16:38:08 +0530

    Create main.tf

---
+++ terraform/modules/security/main.tf
@@ -0,0 +1,71 @@
+resource "aws_security_group" "jenkins" {
+  vpc_id = var.vpc_id
+ 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
+resource "aws_security_group" "sonarqube" {
+  vpc_id = var.vpc_id
+ 
+  ingress {
+    from_port       = 9000
+    to_port         = 9000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jenkins.id]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
+resource "aws_security_group" "nexus" {
+  vpc_id = var.vpc_id
+ 
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
+resource "aws_security_group" "docker" {
+  vpc_id = var.vpc_id
+ 
+  ingress {
+    from_port   = 2375
+    to_port     = 2375
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
